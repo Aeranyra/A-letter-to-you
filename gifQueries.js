39bd5a5732f8b1b94af025e387b925fce73f359d
Aeranyra <yasyastar38@gmail.com>
2026-07-01 08:39:38 +0800

    Add files via upload

---
+++ gifQueries.js
@@ -0,0 +1,27 @@
+// Search terms used to fetch a relevant GIF per command from Tenor.
+// Tweak these any time to change the vibe of what gets pulled back.
+module.exports = {
+  kiss: "anime kiss",
+  hug: "anime hug",
+  pat: "anime headpat",
+  poke: "anime poke",
+  feed: "anime feeding",
+  slap: "anime slap",
+  bonk: "anime bonk",
+  stare: "anime stare",
+  prank: "anime prank",
+  curse: "anime curse",
+  steal: "anime steal",
+  gossip: "anime gossip",
+  blackmail: "anime smirk",
+  investigate: "anime detective",
+  whisper: "anime whisper",
+  omen: "anime tarot card",
+  duel: "anime sword fight",
+  sick: "anime sick in bed",
+  diagnose: "anime doctor examining",
+  treat: "anime nursing care",
+  checkup: "anime doctor checkup",
+  feral: "anime feral eyes",
+  calm: "anime calming down"
+};
